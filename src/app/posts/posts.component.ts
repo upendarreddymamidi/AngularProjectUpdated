1ea2dfd2e7f08d03e36bda59b3aaa79b559b0901
upendarreddymamidi <upendarreddymamidi@gmail.com>
2018-11-07 17:34:00 -0500

    Update posts.component.ts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -17,13 +17,28 @@ export class PostsComponent  {
   constructor(private data: DataService) { }
 
   ngOnclick() {
-  
-    this.data.getPosts().subscribe(
-      data => this.posts$ = data 
-    );
+    document.getElementById("outputDiv").innerHTML = 
+    "<form id='form1' name='form1'Onsubmit  ='Onsubmit()'<td>"
+    document.getElementById("outputDiv").innerHTML += "Title:<input name='name' type='text' id='name'/></td> <br>"
+    document.getElementById("outputDiv").innerHTML += "<td> Body: <input name='address' type='text' id='address'/> <br></td>"
+    document.getElementById("outputDiv").innerHTML +="  <input type='submit' value= 'submit '>" 
+}
+Onsubmit ()
+{
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'form.name',
+      body: 'form.address',
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
 
-
-  }
+}
 
 
 }
