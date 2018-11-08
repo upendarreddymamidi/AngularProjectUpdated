e86368cc18575977717a04efd61380b752b9142b
upendarreddymamidi <upendarreddymamidi@gmail.com>
2018-11-08 13:18:06 -0500

    Update posts.component.ts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -18,27 +18,137 @@ export class PostsComponent  {
 
   ngOnclick() {
     document.getElementById("outputDiv").innerHTML = 
-    "<form id='form1' name='form1'Onsubmit  ='Onsubmit()'<td>"
-    document.getElementById("outputDiv").innerHTML += "Title:<input name='name' type='text' id='name'/></td> <br>"
-    document.getElementById("outputDiv").innerHTML += "<td> Body: <input name='address' type='text' id='address'/> <br></td>"
-    document.getElementById("outputDiv").innerHTML +="  <input type='submit' value= 'submit '>" 
+    "<form (onsubmit) ='formValidation()'><br>"
+    document.getElementById("outputDiv").innerHTML += " <label>Title:</label>  <input type='text' id='title'/><br /> <p id='p1'></p>	"
+    document.getElementById("outputDiv").innerHTML += "  <label>Body</label>  <input type='text' id='body' /><br />   <p id='p2'></p> "
+    //document.getElementById("outputDiv").innerHTML +="<button style='border-radius: 2px;  border: none;font-size: 16px; margin: 4px 2px; cursor: pointer;' onclick= 'formValidation()' > Validate the Form </button>"
+    document.getElementById("outputDiv").innerHTML +="</form>"
+
+
+
+    
+
+    //document.getElementById("outputDiv2").innerHTML +="<button style='border-radius: 2px;  border: none;font-size: 16px; margin: 4px 2px; cursor: pointer;' (click)= 'formValidation()' > Validate the Form </button> </form>"
+
+
+
+  
+
+
+
+   
+   
+    
+  
+  //   <button style='border-radius: 2px;  border: none;font-size: 16px; margin: 4px 2px; 
+  //cursor: pointer;' (click)= 'formValidation()' > Validate the Form </button> </form>
+
 }
-Onsubmit ()
-{
-  fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    body: JSON.stringify({
-      title: 'form.name',
-      body: 'form.address',
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
+
+/// 
+
+
+formValidation(){
+
+	// Make quick references to our fields
+  var title =  document.getElementById('title');
+	var body =  document.getElementById('body');
+	
+	
+	// Check each input in the order that it appears in the form!
+	 if(this.textAlphanumeric(title, "* For title please use alphanumeric only *")){
+		
+		if(this.lengthDefine(title, 1, 24)){
+      return true;
+
     }
-  })
-  .then(response => response.json())
-  .then(json => console.log(json))
+  
+  }
+
+
+
+    
+      
+      if(this.lengthDefinebody(body, 1, 54)){
+		   // if(this.emailValidation(email, "* Please enter a valid email address *")){
+		
+		       // if(this.trueSelection(state, "* Please Choose any one option")){
+					
+				
+						  return true;
+						
+          
+        
+  
+				
+			
+		
+	}
+	
+	
+	return false;
+	
+}
+
+//function that checks whether input text is numeric or not.
+
+ 
+
+
+//function that checks whether input text is an alphabetic character or not.
+
+
+
+
+ textAlphanumeric(inputtext, alertMsg){
+	var alphaExp = /^[0-9a-zA-Z]+$/;
+	if(inputtext.value.match(alphaExp)){
+		return true;
+	}else{
+    //document.getElementById(id).innerText = alertMsg; //this segment displays the validation rule for address
+    document.getElementById('p1').innerText = alertMsg; //this segment displays the validation rule for address
+		inputtext.focus();
+		return false;
+	}
+}
+
+// Function that checks whether the input characters are restricted according to defined by user.
+
+lengthDefine(inputtext, min, max){
+	var uInput = inputtext.value;
+	if(uInput.length >= min && uInput.length <= max){
+		return true;
+	}else{
+	//	document.getElementById(id).innerText = "* Please enter between " +min+ " and " +max+ " characters *"; 
+   document.getElementById('p1').innerText = "* Please enter between " +min+ " and " +max+ " characters *"; 
+    		inputtext.focus();
+		return false;
+	}
+}
+
+
+
+// Function that checks whether the input characters are restricted according to defined by user.
+
+lengthDefinebody(inputtext, min, max){
+	var uInput = inputtext.value;
+	if(uInput.length >= min && uInput.length <= max){
+		return true;
+	}else{
+		//document.getElementById(id).innerText = "* Please enter between " +min+ " and " +max+ " characters *"; 
+   document.getElementById('p2').innerText = "* Please enter between " +min+ " and " +max+ " characters *"; 
+    		inputtext.focus();
+		return false;
+  }
+}
 
 }
 
 
-}
+
+
+
+
+
+
+
