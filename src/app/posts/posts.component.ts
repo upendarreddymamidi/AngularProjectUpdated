050d7006b261141ea6b611c21dedd52cc58e16e1
upendarreddymamidi <upendarreddymamidi@gmail.com>
2018-11-09 20:27:45 -0500

    Update posts.component.ts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -7,143 +7,114 @@ import { ActivatedRoute,RouterModule } from "@angular/router";
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  //styleUrls: ['./posts.component.css']
 })
 export class PostsComponent  {
 
 
   posts$: Object;
-  
+  public createPost = false;
+
   constructor(private data: DataService) { }
 
   ngOnclick() {
-    document.getElementById("outputDiv").innerHTML = 
-    "<form (onsubmit) ='formValidation()'><br>"
-    document.getElementById("outputDiv").innerHTML += " <label>Title:</label>  <input type='text' id='title'/><br /> <p id='p1'></p>	"
-    document.getElementById("outputDiv").innerHTML += "  <label>Body</label>  <input type='text' id='body' /><br />   <p id='p2'></p> "
-    //document.getElementById("outputDiv").innerHTML +="<button style='border-radius: 2px;  border: none;font-size: 16px; margin: 4px 2px; cursor: pointer;' onclick= 'formValidation()' > Validate the Form </button>"
-    document.getElementById("outputDiv").innerHTML +="</form>"
+    //console.log('method');
+    this.createPost = true;
+  }
+
+///
+
+
+  formValidation(){
+    console.log('method1');
+
+    // Make quick references to our fields
+    var title =  document.getElementById('title');
+    var body =  document.getElementById('body');
+
+
+    // Check each input in the order that it appears in the form!
+    if(this.textAlphanumeric(title, "* For title please use alphanumeric only *")) {
+
+      document.getElementById('p1').innerText = "inValid Entry ";
+
+      if (this.lengthDefine(title, 1, 24)) {
+        document.getElementById('p1').innerText = "Valid Entry ";
+
+      }
+    }
 
 
 
-    
-
-    //document.getElementById("outputDiv2").innerHTML +="<button style='border-radius: 2px;  border: none;font-size: 16px; margin: 4px 2px; cursor: pointer;' (click)= 'formValidation()' > Validate the Form </button> </form>"
 
 
 
-  
+
+    if(this.lengthDefinebody(body, 1, 54)){
+      // if(this.emailValidation(email, "* Please enter a valid email address *")){
+
+      // if(this.trueSelection(state, "* Please Choose any one option")){
+
+      document.getElementById('p2').innerText = "Valid Entry";
+
+      //return true;
 
 
 
-   
-   
-    
-  
-  //   <button style='border-radius: 2px;  border: none;font-size: 16px; margin: 4px 2px; 
-  //cursor: pointer;' (click)= 'formValidation()' > Validate the Form </button> </form>
-
-}
-
-/// 
 
 
-formValidation(){
 
-	// Make quick references to our fields
-  var title =  document.getElementById('title');
-	var body =  document.getElementById('body');
-	
-	
-	// Check each input in the order that it appears in the form!
-	 if(this.textAlphanumeric(title, "* For title please use alphanumeric only *")){
-		
-		if(this.lengthDefine(title, 1, 24)){
-      return true;
 
     }
-  
+
+
+    return false;
+
   }
 
-
-
-    
-      
-      if(this.lengthDefinebody(body, 1, 54)){
-		   // if(this.emailValidation(email, "* Please enter a valid email address *")){
-		
-		       // if(this.trueSelection(state, "* Please Choose any one option")){
-					
-				
-						  return true;
-						
-          
-        
-  
-				
-			
-		
-	}
-	
-	
-	return false;
-	
-}
-
-//function that checks whether input text is numeric or not.
-
- 
-
-
-//function that checks whether input text is an alphabetic character or not.
-
-
-
-
- textAlphanumeric(inputtext, alertMsg){
-	var alphaExp = /^[0-9a-zA-Z]+$/;
-	if(inputtext.value.match(alphaExp)){
-		return true;
-	}else{
-    //document.getElementById(id).innerText = alertMsg; //this segment displays the validation rule for address
-    document.getElementById('p1').innerText = alertMsg; //this segment displays the validation rule for address
-		inputtext.focus();
-		return false;
-	}
-}
+  textAlphanumeric(inputtext, alertMsg){
+    var alphaExp = /^[0-9a-zA-Z]+$/;
+    if(inputtext.value.match(alphaExp)){
+      return true;
+    }else{
+      //document.getElementById(id).innerText = alertMsg; //this segment displays the validation rule for address
+      document.getElementById('p1').innerText = alertMsg; //this segment displays the validation rule for address
+      inputtext.focus();
+      return false;
+    }
+  }
 
 // Function that checks whether the input characters are restricted according to defined by user.
 
-lengthDefine(inputtext, min, max){
-	var uInput = inputtext.value;
-	if(uInput.length >= min && uInput.length <= max){
-		return true;
-	}else{
-	//	document.getElementById(id).innerText = "* Please enter between " +min+ " and " +max+ " characters *"; 
-   document.getElementById('p1').innerText = "* Please enter between " +min+ " and " +max+ " characters *"; 
-    		inputtext.focus();
-		return false;
-	}
-}
+  lengthDefine(inputtext, min, max){
+    var uInput = inputtext.value;
+    if(uInput.length >= min && uInput.length <= max){
+      return true;
+    }else{
+      //	document.getElementById(id).innerText = "* Please enter between " +min+ " and " +max+ " characters *";
+      document.getElementById('p1').innerText = "* Please enter between " +min+ " and " +max+ " characters *";
+      inputtext.focus();
+      return false;
+    }
+  }
 
 
 
 // Function that checks whether the input characters are restricted according to defined by user.
 
-lengthDefinebody(inputtext, min, max){
-	var uInput = inputtext.value;
-	if(uInput.length >= min && uInput.length <= max){
-		return true;
-	}else{
-		//document.getElementById(id).innerText = "* Please enter between " +min+ " and " +max+ " characters *"; 
-   document.getElementById('p2').innerText = "* Please enter between " +min+ " and " +max+ " characters *"; 
-    		inputtext.focus();
-		return false;
+  lengthDefinebody(inputtext, min, max){
+    var uInput = inputtext.value;
+    if(uInput.length >= min && uInput.length <= max){
+      return true;
+    }else{
+      //document.getElementById(id).innerText = "* Please enter between " +min+ " and " +max+ " characters *";
+      document.getElementById('p2').innerText = "* Please enter between " +min+ " and " +max+ " characters *";
+      inputtext.focus();
+      return false;
+    }
   }
-}
 
 }
-
 
 
 
